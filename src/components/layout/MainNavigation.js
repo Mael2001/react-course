83c2fd310659290180685d6fb194a187bfb81e1d
Mario Flores <mario.flores@uvltd.tech>
2023-08-10 11:00:43 -0600

    Adding component styling

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom/cjs/react-router-dom";
+import classes from "./MainNavigation.module.css"
 
 function MainNavigation() {
-	return <header>
-		<div> React Meetups </div>
-		<nav>
-			<ul>
-				<Link to='/'>All Meetups</Link>
-			</ul>
-			<ul>
-				<Link to='/new-meetup'>Add New Meetup</Link>
-			</ul>
-			<ul>
-				<Link to='/favorites'>My Favorites</Link>
-			</ul>
-		</nav>
-	</header>
+	return (
+		<header className={classes.header}>
+			<div className={classes.logo}> React Meetups </div>
+			<nav>
+				<ul>
+					<li>
+						<Link to='/'>All Meetups</Link>
+					</li>
+					<li>
+						<Link to='/new-meetup'>Add New Meetup</Link>
+					</li>
+					<li>
+						<Link to='/favorites'>My Favorites</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	)
 }
 
 export default MainNavigation;
