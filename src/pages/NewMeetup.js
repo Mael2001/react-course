dbb526f3a0b1446fd9b612a87357d0d44057a7e3
Mario Flores <mario.flores@uvltd.tech>
2023-08-15 15:17:41 -0600

    Adding Firebase Call

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -3,13 +3,24 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
 
 	function addMeetupHandler(meetupData) {
-
+		fetch(
+			'https://react-e38f1-default-rtdb.firebaseio.com/meetups.json',
+			{
+				method: 'POST',
+				mode: "cors",
+				body: JSON.stringify(meetupData),
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': 'origin'
+				}
+			}
+		);
 	}
 
 	return (
 		<section>
 			<h1>Add New Meetup</h1>
-			<NewMeetupForm onAddMetup={addMeetupHandler} />
+			<NewMeetupForm onAddMeetup={addMeetupHandler} />
 		</section>
 	)
 }
